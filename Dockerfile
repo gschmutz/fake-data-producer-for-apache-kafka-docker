8321ae04a5e5e7eeef39e9f3f47a241b6d81f33a
Guido Schmutz <schmutz68@gmail.com>
2025-06-08 17:42:29 +0200

    add environment variable support and build & push docker image to github

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,6 @@ FROM python:3.10
 WORKDIR /usr/src/app
 
 COPY ./run.sh .
-COPY ./conf ./conf
 RUN apt-get update &&\
     apt-get install -y expect &&\
     apt-get install -y jq
--- run.sh
+++ run.sh
@@ -1,29 +1,9 @@
-# Read env parameters
-. conf/env.conf
-
-# Get Hostname and Port
-HOSTNAME=$(avn --auth-token $TOKEN service get $SERVICE_NAME --project $PROJECT_NAME --json | jq -r '.components[] | select(.component=="kafka").host') 
-PORT=$(avn --auth-token $TOKEN service get $SERVICE_NAME --project $PROJECT_NAME --json | jq -r '.components[] | select(.component=="kafka").port')
-echo $HOSTNAME $PORT
-
-if [ $PRIVATELINK == "YES" ]; then
-  HOSTNAME=$(avn --auth-token $TOKEN service get $SERVICE_NAME --project $PROJECT_NAME --json | jq -r '.components[] | select(.route=="privatelink" and .component=="kafka").host') 
-  PORT=$(avn --auth-token $TOKEN service get $SERVICE_NAME --project $PROJECT_NAME --json | jq -r '.components[] | select(.route=="privatelink" and .component=="kafka").port')
-fi
-
-# Get Certificates
-rm -rf ./certs
-avn --auth-token $TOKEN service user-creds-download $SERVICE_NAME  --project $PROJECT_NAME --username avnadmin -d ./certs
-
-# Create topic
-avn --auth-token $TOKEN service topic-create $SERVICE_NAME $TOPIC --project $PROJECT_NAME --partitions $PARTITIONS --replication $REPLICATION
-
-# Gererate Data
+# Generate Data
 python python-fake-data-producer-for-apache-kafka/main.py --cert-folder ./certs/ \
-  --host $HOSTNAME \
-  --port $PORT \
-  --topic-name $TOPIC \
-  --nr-messages $NR_MESSAGES \
-  --max-waiting-time $MAX_TIME \
-  --subject $SUBJECT	\
-  --security-protocol $SECURITY
+  --host ${BOOTSTRAP_SERVER:-localhost} \
+  --port ${PORT:-9092} \
+  --topic-name ${TOPIC_NAME:-pizza} \
+  --nr-messages ${NUMBER_OF_MESSAGES:-0} \
+  --max-waiting-time ${MAX_TIME:-0} \
+  --subject ${SUBJECT:-pizza}	\
+  --security-protocol ${SECURITY_PROTOCOL:-PLAINTEXT}
