10cc48bfbba80e3b1788fab67a151764f5063980
Guido Schmutz <schmutz68@gmail.com>
2025-06-08 18:18:06 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,6 +3,8 @@ python python-fake-data-producer-for-apache-kafka/main.py --cert-folder ./certs/
   --host ${BOOTSTRAP_SERVER:-localhost} \
   --port ${PORT:-9092} \
   --topic-name ${TOPIC_NAME:-pizza} \
+  --partitions ${TOPIC_PARTITIONS:-1} \
+  --repliation ${TOPIC_REPLICATION_FACTOR:-1} \
   --nr-messages ${NUMBER_OF_MESSAGES:-0} \
   --max-waiting-time ${MAX_TIME:-0} \
   --subject ${SUBJECT:-pizza}	\
